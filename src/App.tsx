7dd9567bb0cc35ba327866a81961bdf08f29e617
qian wang <1510106069@qq.com>
2022-08-15 15:11:06 +0800

    feat: Button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Button, {ButtonSize, ButtonType} from './components/Button/button'
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Button onClick={e => alert(123)}>hello</Button>
+      <Button disabled>hello</Button>
+      <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+      <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>hello</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com">Baidu Link</Button>
+      <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>Baidu Link</Button>
+    </div>
   );
 }
 
