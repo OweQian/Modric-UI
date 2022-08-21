49623627de990930334910a5c4432a276962d03f
qian wang <1510106069@qq.com>
2022-08-21 22:57:17 +0800

    feat: Form storybook

--- src/components/Form/form.tsx
+++ src/components/Form/form.tsx
@@ -5,10 +5,14 @@ import useStore, { FormState } from "./useStore";
 export type RenderProps = (form: FormState) => React.ReactNode
 
 export interface FormProps {
+  /**表单名称，会作为表单字段 id 前缀使用 */
   name?: string;
+  /**表单默认值，只有初始化以及重置时生效 */
   initialValues?: Record<string, any>;
   children?: React.ReactNode | RenderProps;
+  /**提交表单且数据验证成功后回调事件 */
   onFinish?: (values: Record<string, any>) => void;
+  /**提交表单且数据验证失败后回调事件 */
   onFinishFailed?: (values: Record<string, any>, errors: Record<string, ValidateError[]>) => void;
 }
 
--- src/components/Form/formItem.tsx
+++ src/components/Form/formItem.tsx
@@ -4,13 +4,20 @@ import { FormContext } from './form';
 import { CustomRule } from './useStore'
 export type SomeRequired<T, K extends keyof T> = Required<Pick<T, K>> & Omit<T, K>
 export interface FormItemProps {
+  /**字段名 */
   name: string;
+  /**label 标签的文本 */
   label?: string;
   children?: React.ReactNode;
+  /**子节点的值的属性，如 checkbox 的是 'checked' */
   valuePropName?: string;
+  /**设置收集字段值变更的时机 */
   trigger?: string;
+  /**设置如何将 event 的值转换成字段值 */
   getValueFromEvent?: (event: any) => any;
+  /**校验规则，设置字段的校验逻辑。请看 async validator 了解更多规则 */
   rules?: CustomRule[];
+  /**设置字段校验的时机 */
   validateTrigger?: string;
 }
 
